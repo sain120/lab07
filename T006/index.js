6ab7b03fa2b167867601c29020f4b387ade71d44
sain120 <alejandro.camacho.arias@gmail.com>
2020-04-08 12:22:22 +0000

    folder uploaded

---
+++ T006/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 80;
+
+var contacts = [
+	{ 
+		name: "peter",
+		phone: 123456	
+	},
+	{ 
+		name: "pablo",
+		phone: 789456	
+	}
+];
+
+const BASE_API_URL = "/api/v1";
+
+
+
+app.get(BASE_API_URL+"/contacts", (req,res) =>{
+	res.send(JSON.stringify(contacts,null,2));
+});
+
+app.post(BASE_API_URL+"/contacts",(req,res) =>{
+	contacts.push(req.body); 
+	res.sendStatus(201,"CREATED");
+});
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
